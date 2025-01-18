9cd7b4ad6b69e5f8a1e9f8be937ceca17bf45620
Arda Aydın <ardaaydin@Arda-MacBook-Air.local>
2025-01-19 00:20:39 +0300

    commit

---
+++ imageProcessor.py
@@ -0,0 +1,20 @@
+# imageProcessor.py
+
+from ultralytics import YOLO
+import cv2
+
+# Load YOLO model
+model = YOLO("yolo11n-seg.pt")
+
+def process_frame(cap):
+    ret, frame = cap.read()
+    if not ret:
+        return None
+    
+    # YOLO inference
+    results = model(frame)
+    
+    # Render the results to the frame
+    output_frame = results[0].plot()  # plot() ile çizim yapıyoruz
+    
+    return output_frame
